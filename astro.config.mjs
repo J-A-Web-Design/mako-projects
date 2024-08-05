283a5f8cee0c5aa3bace28b80a2a2372e6f1fda4
Jeffrey A <agyepong99@gmail.com>
2024-08-05 17:59:33 -0600

    bookshop

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
+import bookshop from '@bookshop/astro-bookshop';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    bookshop(),
+    tailwind()]
 });
