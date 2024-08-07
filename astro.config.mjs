73a020d716d69ab3fbf1a5d3f26156f4ae9a4002
Jeffrey A <agyepong99@gmail.com>
2024-08-06 19:34:29 -0600

    astro image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
+import { astroImageTools } from "astro-imagetools";
 import { defineConfig } from 'astro/config';
 import bookshop from '@bookshop/astro-bookshop';
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    bookshop(),
-    tailwind()]
+  integrations: 
+  [
+    astroImageTools,
+    bookshop(), 
+    tailwind(), 
+    sitemap()
+  ]
 });
