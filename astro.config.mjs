554cfa9c6ea1d53cdd98aa24b73368a8bad2abef
Jeffrey A <agyepong99@gmail.com>
2024-08-18 20:51:53 -0600

    robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import bookshop from '@bookshop/astro-bookshop';
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: 
-  [
-    tailwind(), 
-    sitemap(),
-    bookshop()
-  ]
+  integrations: [tailwind(), sitemap(), bookshop(), robotsTxt()]
 });
