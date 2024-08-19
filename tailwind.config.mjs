6f4a61e47a42cf17f0fb5c38529d6d1a3ed933c8
Jeffrey A <agyepong99@gmail.com>
2024-08-18 20:31:32 -0600

    Update tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-const withMT = require("@material-tailwind/react/utils/withMT");
-
-export default withMT({
+export default ({
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
