88536615d8475de8b20ab465e7784ffa960dc393
Jeffrey A <44594550+jeffrey-agyepong@users.noreply.github.com>
2024-08-20 07:22:22 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import bookshop from '@bookshop/astro-bookshop';
 import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
 
 import robotsTxt from "astro-robots-txt";
 
@@ -9,7 +8,7 @@ import robotsTxt from "astro-robots-txt";
 export default defineConfig({
   site: 'https://makoprojects.ca',
 
-  integrations: [tailwind(), sitemap(), bookshop(), robotsTxt({
+  integrations: [tailwind(), bookshop(), robotsTxt({
     sitemap: false,
   })]
 });
