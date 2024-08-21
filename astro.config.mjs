1beebf1959852c7a9447f857fffbc2453f2b5ee5
Jeffrey A <agyepong99@gmail.com>
2024-08-21 00:49:10 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://makoprojects.ca',
-
-<<<<<<< HEAD
   integrations: [tailwind(), sitemap(), bookshop()]
 });
-=======
-  integrations: [tailwind(), bookshop(), robotsTxt({
-    sitemap: false,
-  })]
-});
->>>>>>> 9893e4f840016b7761bf28f4499442275ce76ea9
+
+
