7142ca53796dd31ad941f7384510e1b97872808f
Jeffrey A <agyepong99@gmail.com>
2024-08-21 00:47:27 -0600

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,9 @@ import bookshop from '@bookshop/astro-bookshop';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
-import robotsTxt from "astro-robots-txt";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://makoprojects.ca',
 
-  integrations: [tailwind(), sitemap(), bookshop(), robotsTxt({
-    sitemap: false,
-  })]
+  integrations: [tailwind(), sitemap(), bookshop()]
 });
