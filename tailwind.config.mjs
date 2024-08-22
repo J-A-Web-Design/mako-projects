2087eda992fcdaa4234d764046dd70a4a47dfbe9
Jeffrey A <agyepong99@gmail.com>
2024-08-21 22:29:11 -0600

    brightness

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default ({
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			brightness: {
+				'50': '.5',
+				'75': '.75',
+				'90': '.9',
+				'100': '1',
+				'125': '1.25',
+				'150': '1.5',
+				'200': '2',
+			  }
+		},
 	},
 	plugins: [],
 });
