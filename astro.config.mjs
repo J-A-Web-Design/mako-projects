7eca9bdf602a800794a6f4592bea37ff2a982da3
Jeffrey A <agyepong99@gmail.com>
2024-08-22 22:16:57 -0600

    google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 import { defineConfig } from 'astro/config';
 import bookshop from '@bookshop/astro-bookshop';
 import tailwind from "@astrojs/tailwind";
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://makoprojects.ca',
-  integrations: [tailwind(), bookshop()]
+  integrations: [
+    tailwind(),
+    bookshop(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ]
 });
 
 
